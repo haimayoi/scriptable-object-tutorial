59e260a3dd6992f8e73818c593ff4f0397bb08bb
Hải Vân <k34ctqer@gmail.com>
2025-11-08 21:44:22 +0700

    done part 1

--- Assets/RW/Scripts/Sword.cs
+++ Assets/RW/Scripts/Sword.cs
@@ -32,5 +32,16 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
+    [SerializeField]
+    private SwordData swordData; // 1
+
+    private void OnMouseDown() // 2
+    {
+        Debug.Log(swordData.SwordName); // 3
+        Debug.Log(swordData.Description); // 3
+        Debug.Log(swordData.Icon.name); // 3
+        Debug.Log(swordData.GoldCost); // 3
+        Debug.Log(swordData.AttackDamage); // 3
+    }
 
 }
